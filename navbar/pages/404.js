55b15905df0bff79c272a0398021725344087e8c
smit312 <smit.coder31@gmail.com>
2022-03-11 13:53:19 +0530

    added custom error page

---
+++ navbar/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <center>
+      <h1> 404 | This page could not be found.</h1>
+    </center>
+  );
+};
+
+export default ErrorPage;
