fec6387da6d0dc5d27b451f14a5740685841a469
smit312 <smit.coder31@gmail.com>
2022-03-11 15:59:13 +0530

    add image via external link

--- navbar/pages/home.js
+++ navbar/pages/home.js
@@ -7,7 +7,14 @@ const home = () => {
     <div>
       <Navbar />
       <h1>home Page</h1>
-      <Image src="/img.png" width="500" height="300"></Image>
+      <Image src="/img.png" width="500" height="300"></Image>{" "}
+      {/* internal image  */}
+      <Image
+        src="https://images.pexels.com/photos/257897/pexels-photo-257897.jpeg?/"
+        width="500"
+        height="300"
+      ></Image>{" "}
+      {/* external image */}
     </div>
   );
 };
