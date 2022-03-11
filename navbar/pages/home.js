1ed439ae53213e4b6fb6b527777f42edea7edb6f
smit312 <smit.coder31@gmail.com>
2022-03-11 15:47:47 +0530

    add internal images

--- navbar/pages/home.js
+++ navbar/pages/home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Image from "next/image";
+import Navbar from "../Components/Navbar";
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Navbar />
+      <h1>home Page</h1>
+      <Image src="/img.png" width="500" height="300"></Image>
+    </div>
+  );
 };
 
 export default home;
