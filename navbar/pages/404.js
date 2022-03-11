80db4fb21544a8d766e2f2844ce56c3b4bb5fd12
smit312 <smit.coder31@gmail.com>
2022-03-11 14:22:53 +0530

    add event on error page

--- navbar/pages/404.js
+++ navbar/pages/404.js
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import React from "react";
-
+import { useRouter } from "next/router";
 const ErrorPage = () => {
+  const router = useRouter();
   return (
     <center>
       <h1> 404 | This page could not be found.</h1>
-      <Link href="/">
-        <a style={{ color: "blue" }}> Back to home page</a>
-      </Link>
+      {/* <Link href="/"> */}
+      <a style={{ color: "blue" }} onClick={() => router.push("/")}>
+        Back to home page
+      </a>
+      {/* </Link> */}
     </center>
   );
 };
