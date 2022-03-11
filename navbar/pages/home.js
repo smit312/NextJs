8113a3b3f386752942f8562d4ffbff9e91a26b6d
smit312 <smit.coder31@gmail.com>
2022-03-11 16:18:57 +0530

    add head tag

--- navbar/pages/home.js
+++ navbar/pages/home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import Navbar from "../Components/Navbar";
-
+import Head from "next/head";
 const home = () => {
   return (
     <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
       <Navbar />
       <h1>home Page</h1>
       <Image src="/img.png" width="500" height="300"></Image>{" "}
