8788fa50dd0253c505abcc1afec1528306be439d
smit312 <smit.coder31@gmail.com>
2022-03-16 16:59:28 +0530

    added : data and style

--- recipeapp/pages/index.js
+++ recipeapp/pages/index.js
@@ -17,13 +17,21 @@ export async function getStaticProps() {
 
 export default function Recipes({ recipes }) {
   console.log(recipes);
+
   return (
     <div className="recipe-list">
-      Recipe List
-      <hr />
-      {recipes.map((recipe) => {
-        return <RecipeCard key={recipe.sys.id} recipe={recipe} />;
-      })}
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+      <style jsx>
+        {`
+          .recipe-list {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+          }
+        `}
+      </style>
     </div>
   );
 }
